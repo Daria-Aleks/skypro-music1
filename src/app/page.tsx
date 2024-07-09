84fc17967703742987dbf927c8a949c5e5b96147
Александра <aleksandra@MacBook-Pro-Andrej-2.local>
2024-07-09 15:30:35 +0300

    dz1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,14 @@
-// import Image from "next/image";
 import Sidebar from "@/components/Sidebar/Sidebar";
-// import Track from "@/components/Track/Track";
 import Bar from "@/components/Bar/Bar";
 import Nav from "@/components/Nav/Nav";
 import CenterBlock from "@/components/CetnerBlock/CenterBlock";
-
+import styles from './page.module.css'
 export default function Home() {
   return (
     <>
-    <div className="wrapper">
-      <div className="container">
-        <main className="main">
+    <div className={styles.wrapper}>
+      <div className={styles.container}>
+        <main className={styles.main}>
           <Nav/>
           <CenterBlock/>
           <Sidebar/>
